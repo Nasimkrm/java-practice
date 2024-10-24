ffba6e83c7c141d2e3dbe7784ab769cc49599e47
Hamza <hamza46awais@gmail.com>
2024-10-24 10:12:12 +0100

    EDIT: actually completed TASK 1.1

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,18 @@
 public class Main {
     public static void main(String[] args) {
         int i = 0;
-        while (i < 10) {
+//        while (i < 10) {
+//            System.out.println("i = " + i);
+//            i++;
+//        }
+//
+//        i = 0;
+//        i = 0;
+        do {
             System.out.println("i = " + i);
             i++;
         }
+        while (i < 10);
 
     }
 }
