c60772c86af0939f7ab93b15ed45356b093f178e
Nasim <nassim.karimi.nk@gmail.com>
2024-10-24 10:18:10 +0100

    EDIT: completed first half of 1.2

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@
 public class Main {
     public static void main(String[] args) {
         int i = 0;
+        int j = 10;
 //        while (i < 10) {
 //            System.out.println("i = " + i);
 //            i++;
@@ -9,11 +10,20 @@ public class Main {
 //
 //        i = 0;
 //        i = 0;
+//        do {
+//            System.out.println("i = " + i);
+//            i++;
+//        }
+//        while (i < 10);
+
+
         do {
             System.out.println("i = " + i);
+            System.out.println("j = " + j);
+
             i++;
-        }
-        while (i < 10);
+            j--;
+        } while (j > 0 && i < 10);
 
     }
 }
